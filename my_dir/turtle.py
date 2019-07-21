8efd77ef692639254c0e88be9b1d34ae32203c03
nir21-meet <nir21@meet.mit.edu>
2019-07-21 09:32:45 -0400

    Stopping here

---
+++ my_dir/turtle.py
@@ -0,0 +1,15 @@
+import turtle
+
+# Everything that comes after the # is a 
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+
+
+# ...and end it before the next line.
+# turtle.mainloop() tells the turtle to do all
+# the turtle commands above it and paint it on the screen.
+# It always has to be the last l
+turtle.goto(100,200)
+turtle.mainloop()
